1fa0451ccddee39d543c4780752a0778a4e503e0
rheeseula <rheeseula@dgsw.hs.kr>
2023-12-18 20:22:17 +0900

    🖥 | 2. 주석 (Comment)

---
+++ src/CommentJava.java
@@ -0,0 +1,18 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        // 주석 : 사람이 읽기 위해 사용
+
+        // Single Line Comment
+
+        // "Hello Java 1." 을 출력합니다
+        System.out.println("Hello Java 1.");
+        // System.out.println("Hello Java 2.");
+
+        // Multi Line Comment
+
+        /*
+        System.out.println("Hello Java 3.");
+        System.out.println("Hello Java 4.");
+        */
+    }
+}
