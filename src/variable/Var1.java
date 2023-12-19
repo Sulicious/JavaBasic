1827f04b6299db3e3c4470a8a81f36e7d111ae3e
rheeseula <rheeseula@dgsw.hs.kr>
2023-12-19 14:12:07 +0900

    🙂 | 3. 변수 시작

---
+++ src/variable/Var1.java
@@ -0,0 +1,15 @@
+// 패키지는 자바 파일을 구분하기 위한 폴더
+
+/* 패키지를 만들었다면 해당 패키지에 들어가는 클래스는
+ 아래와 같이 소속된 패키지를 선언해야한다.
+*/
+package variable;
+
+// 클래스를 만들 때는 관례상으로 파일의 첫 문자를 대문자로 한다.
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+    }
+}
---
+++ src/variable/Var2.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        // 변수는 그릇이다.
+
+        int a; // 선언
+        a = 10; // 초기화 (변수에 처음으로 값을 넣었을 때)
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
