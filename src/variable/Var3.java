713bbfb58a9a758e861a12d19c4ab188bff4e40c
rheeseula <rheeseula@dgsw.hs.kr>
2023-12-19 14:32:40 +0900

    😏 | 4. 변수 값 변경

---
+++ src/variable/Var3.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a; // 선언
+        a = 10; // 초기화
+
+        System.out.println(a);
+
+        a = 50; // 변경 (10 -> 50)
+
+        System.out.println(a);
+    }
+}
