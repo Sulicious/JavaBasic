0a01e482710b4e4fafd6e2af96f8e42acb5daac3
rheeseula <rheeseula@dgsw.hs.kr>
2023-12-31 20:04:13 +0900

    👌 | 변수 타입 - 1

---
+++ src/variable/Var7.java
@@ -0,0 +1,30 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        // 자료형 - type
+
+        /*
+            리터럴 - Literal
+
+            개발자가 프로그램에
+            고정적으로 넣은 값을
+            리터럴이라고 한다.
+        */
+        int a = 100; // 정수 리터럴
+        double b = 10.5; // 실수 리터럴
+        boolean c = true; // true or false 리터럴
+        char d = 'A'; // 문자 하나 ( 작은 따옴표 ) 리터럴
+        String e = "Hello Java"; // 문자 여러개 ( 큰 따옴표 ) 리터럴
+
+        // 타입이 맞지 않음
+        // int z = "백 원";
+
+        // Cmd + D : Duplicate Line ( 한 줄 복사 )
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
