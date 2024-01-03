f0872514a9821629302ff4c27ddf11baaa97bf0f
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-03 16:06:02 +0900

    🌦 | 연산자 우선순위

---
+++ src/operator/Operator4.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class Operator4 {
+    public static void main(String[] args) {
+        int sum3 = 2 * 2 + 3 * 3;
+
+        // 애매하면 괄호를 쓰자.
+        int sum4 = (2 * 2) + (3 * 3);
+
+        System.out.println("sum3 = " + sum3);
+        System.out.println("sum4 = " + sum4);
+    }
+}
